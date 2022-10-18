cdfe37fcdaf4ce4e5cf991688ccc9d1310115c2b
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-18 12:37:31 -0500

    Add inputs to NuevoCliente

--- crudnative/views/NuevoCliente.js
+++ crudnative/views/NuevoCliente.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {TextInput, Button, Headline} from 'react-native-paper';
 import globalStyles from '../styles/global';
 
 const NuevoCliente = () => {
+  const [nombre, guardarNombre] = useState('');
+  const [telefono, guardarTelefono] = useState('');
+  const [correo, guardarCorreo] = useState('');
+  const [empresa, guardarEmpresa] = useState('');
+
   return (
     <View style={globalStyles.contenedor}>
       <Headline style={globalStyles.titulo}>Añadir Nuevo Cliente</Headline>
+      <TextInput
+        label="Nombre"
+        placeholder="Esteban"
+        onChangeText={texto => guardarNombre(texto)}
+        value={nombre}
+        style={styles.input}
+      />
+      <TextInput
+        label="Teléfono"
+        placeholder="9999999999"
+        onChangeText={texto => guardarTelefono(texto)}
+        value={telefono}
+        style={styles.input}
+      />
+      <TextInput
+        label="Correo"
+        placeholder="correo@correo.com"
+        onChangeText={texto => guardarCorreo(texto)}
+        value={correo}
+        style={styles.input}
+      />
+      <TextInput
+        label="Empresa"
+        placeholder="Nombre Empresa"
+        onChangeText={texto => guardarEmpresa(texto)}
+        value={empresa}
+        style={styles.input}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 20,
+    backgroundColor: 'transparent',
+  },
+});
 
 export default NuevoCliente;
