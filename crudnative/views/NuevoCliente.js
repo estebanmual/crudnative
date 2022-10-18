1bc338c700acaceadbdecad75b2e41b6f5d79dd5
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-18 12:27:16 -0500

    Add global styles

--- crudnative/views/NuevoCliente.js
+++ crudnative/views/NuevoCliente.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {TextInput, Button, Headline} from 'react-native-paper';
+import globalStyles from '../styles/global';
 
 const NuevoCliente = () => {
-  return <Text>Desde NuevoCliente</Text>;
+  return (
+    <View style={globalStyles.contenedor}>
+      <Headline style={globalStyles.titulo}>Añadir Nuevo Cliente</Headline>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({});
 
 export default NuevoCliente;
