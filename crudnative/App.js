4f711a9bca43f34deb566018bbcf4ef95c189567
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-18 12:05:34 -0500

    Added react-paper

--- crudnative/App.js
+++ crudnative/App.js
@@ -13,16 +13,33 @@ import Inicio from './views/Inicio';
 import DetallesCliente from './views/DetallesCliente';
 import NuevoCliente from './views/NuevoCliente';
 
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774F2',
+    accent: '#0655BF',
+  },
+};
+
 const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Navigator
+          initialRouteName="Inicio"
+          screenOptions={{
+            headerStyle: {backgroundColor: theme.colors.primary},
+            headerTintColor: theme.colors.surface,
+            headerTitleStyle: {fontWeight: 'bold'},
+          }}>
           <Stack.Screen name="Inicio" component={Inicio} />
           <Stack.Screen
             name="NuevoCliente"
