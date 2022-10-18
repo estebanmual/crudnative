3cbd2434a3e51f761430567cc98037222e6322dd
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-18 13:39:19 -0500

    Add header condicional

--- crudnative/views/Inicio.js
+++ crudnative/views/Inicio.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {List, Headline} from 'react-native-paper';
+import globalStyles from '../styles/global';
 
 const Inicio = () => {
-  return <Text> Inicio </Text>;
+  const [clientes, guardarClientes] = useState([]);
+  useEffect(() => {
+    const obtenerClienteApi = async () => {
+      try {
+        const resultado = await fetch('http://10.0.2.2:3000/clientes');
+        const clientes = await resultado.json();
+        guardarClientes(clientes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerClienteApi();
+  }, []);
+
+  return (
+    <View style={globalStyles.contenedor}>
+      <Headline style={globalStyles.titulo}>
+        {clientes.length > 0 ? 'Clientes' : 'Aún no hay clientes'}
+      </Headline>
+      <FlatList
+        data={clientes}
+        keyExtractor={cliente => cliente.id.toString()}
+        renderItem={({item}) => (
+          <List.Item title={item.nombre} description={item.empresa} />
+        )}
+      />
+    </View>
+  );
 };
 
 export default Inicio;
